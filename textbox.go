41f6e171217e14d269de52b796bc527c03a81d45
reus <reusee@gmail.com>
2014-12-09 12:14:43 +0800

    add event and main loops

--- textbox.go
+++ textbox.go
@@ -1,14 +1,52 @@
 package textbox
 
-import "github.com/nsf/termbox-go"
+import (
+	"sync"
+
+	"github.com/nsf/termbox-go"
+)
+
+var (
+	closed    chan struct{}
+	closeOnce sync.Once
+)
+
+func init() {
+	closed = make(chan struct{})
+}
 
 func Init() error {
 	if err := termbox.Init(); err != nil {
 		return err
 	}
+	events := make(chan *termbox.Event)
+	go func() {
+		for {
+			ev := termbox.PollEvent()
+			switch ev.Type {
+			case termbox.EventError:
+				Close()
+				return
+			default:
+				events <- &ev
+			}
+		}
+	}()
+	go func() {
+		select {
+		case ev := <-events:
+			_ = ev
+			//TODO
+		case <-closed:
+			return
+		}
+	}()
 	return nil
 }
 
 func Close() {
-	termbox.Close()
+	closeOnce.Do(func() {
+		close(closed)
+		termbox.Close()
+	})
 }
