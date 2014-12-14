a17a949b6bb3e71ad5e2986f6659194a48374949
reus <reusee@gmail.com>
2014-12-14 13:48:33 +0800

    implement BoxList.Append/InsertAfter/InsertBefore

--- box_list.go
+++ box_list.go
@@ -15,10 +15,18 @@ func NewBoxList() *BoxList {
 }
 
 func (l *BoxList) Append(boxes ...*Box) {
+	for _, box := range boxes {
+		elem := l.l.PushBack(box)
+		l.m[box] = elem
+	}
 }
 
 func (l *BoxList) InsertAfter(target, box *Box) {
+	elem := l.l.InsertAfter(box, l.m[target])
+	l.m[box] = elem
 }
 
 func (l *BoxList) InsertBefore(target, box *Box) {
+	elem := l.l.InsertBefore(box, l.m[target])
+	l.m[box] = elem
 }
